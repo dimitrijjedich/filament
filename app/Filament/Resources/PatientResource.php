e6deb3fdfc1748a7d3b3ef7c851a5b4f8a3f97d1
dimitrijjedich <dimitrij@startup-werk.de>
2024-02-26 09:40:49 +0100

    make table sortable by date_of_birth

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -60,7 +60,7 @@ class PatientResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')->searchable(),
                 Tables\Columns\TextColumn::make('type'),
-                Tables\Columns\TextColumn::make('date_of_birth'),
+                Tables\Columns\TextColumn::make('date_of_birth')->sortable(),
                 Tables\Columns\TextColumn::make('owner.name')->searchable(),
             ])
             ->filters([
