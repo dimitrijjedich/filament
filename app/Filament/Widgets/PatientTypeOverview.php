cd2524f75f6028a9cd90b4d70df647cb171e5be1
dimitrijjedich <dimitrij@startup-werk.de>
2024-02-27 09:46:19 +0100

    add Stat instances to getStats method of new widget for Patients count by type

--- app/Filament/Widgets/PatientTypeOverview.php
+++ app/Filament/Widgets/PatientTypeOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Patient;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +11,9 @@ class PatientTypeOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Cats', Patient::query()->where('type', 'cat')->count()),
+            Stat::make('Dogs', Patient::query()->where('type', 'dog')->count()),
+            Stat::make('Rabbits', Patient::query()->where('type', 'rabbit')->count()),
         ];
     }
 }
