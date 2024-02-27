4c32fc81e6bb98decd46c1f64add03b6ae1e11d3
dimitrijjedich <dimitrij@startup-werk.de>
2024-02-27 09:58:04 +0100

    add data to chart

--- app/Filament/Widgets/TreatmentsChart.php
+++ app/Filament/Widgets/TreatmentsChart.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Treatment;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class TreatmentsChart extends ChartWidget
 {
@@ -10,8 +13,21 @@ class TreatmentsChart extends ChartWidget
 
     protected function getData(): array
     {
+        $data = Trend::model(Treatment::class)
+            ->between(
+                now()->subYear(),
+                now(),
+            )
+            ->perMonth()
+            ->count();
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Treatments',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
         ];
     }
 
