16452ce80898a243f9956aa3e7a5991588967c0a
dimitrijjedich <dimitrij@startup-werk.de>
2024-02-27 10:47:23 +0100

    add canAccess check example to Settings Page

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Actions\Action;
 use Filament\Pages\Page;
 
 class Settings extends Page
@@ -9,4 +10,9 @@ class Settings extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.settings';
+
+    public static function canAccess(): bool
+    {
+        return true;
+    }
 }
